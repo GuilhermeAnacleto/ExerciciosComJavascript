207abe107c0e68dc196ec0869b8f12945f2afebc
Guilherme Anacleto <131193068+GuilhermeAnacleto@users.noreply.github.com>
2023-05-18 17:22:35 -0300

    Delete Variáveis 24 04 directory

--- Variáveis 24 04/Exercicio1.js
+++
@@ -1,7 +0,0 @@
-const nome = prompt (" Informe seu nome: ")
-const idade = Number (prompt (" Informe sua idade: "))
-
-console.log(typeof nome)
-console.log(typeof idade)
-
-console.log(" Bom dia! ", nome, " você tem ", idade, " anos ")
--- Variáveis 24 04/Exercicio4Desafio.js
+++
@@ -1,5 +0,0 @@
-const numero1 = Number(prompt(" Informe um número: "))
-const numero2 = Number(prompt(" Informe um segundo número"))
-
-console.log(" A soma dos 2 números é: ", numero1 + numero2)
-console.log(" A multiplicação dos 2 números é: ", numero1 * numero2)
